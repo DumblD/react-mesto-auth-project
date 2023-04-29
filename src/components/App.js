8a05c8c95dd0b1724fd1e42efd6766a6fe80a8ff
Artyom Zinin <NicYKN@yandex.ru>
2023-04-29 23:17:17 +0300

    fix: all popups moved to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './Header.js';
 import Main from './Main.js';
+import PopupWithForm from './PopupWithForm.js';
+import ImagePopup from './ImagePopup.js';
 import Footer from './Footer.js';
 
 
@@ -54,12 +56,101 @@ function App() {
         onEditProfile={handleEditProfileClick}
         onAddPlace={handleAddPlaceClick}
         onCardClick={handleCardClick}
-        onClose={closeAllPopups}
-        isEditAvatarPopupOpen={isEditAvatarPopupOpen}
-        isEditProfilePopupOpen={isEditProfilePopupOpen}
-        isAddPlacePopupOpen={isAddPlacePopupOpen}
-        selectedCardImg={selectedCardImg}
         />
+        <PopupWithForm
+          name="avatarUpdateForm"
+          title="Обновить аватар"
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+          submitButtonText="Сохранить">
+          <fieldset className="popup__input-container">
+            <input
+              type="url"
+              name="avatarLink"
+              id="avatarLink"
+              className="popup__input popup__input_el_avatar-link"
+              required
+              placeholder="Ссылка на картинку"
+            />
+            <span className="popup__error avatarLink-error"></span>
+          </fieldset>
+        </PopupWithForm>
+
+        <PopupWithForm
+          name="profileEditForm"
+          title="Редактировать профиль"
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+          submitButtonText="Сохранить">
+          <fieldset className="popup__input-container">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="popup__input popup__input_el_name"
+              required
+              minLength="2"
+              maxLength="40"
+              placeholder="Введите имя"
+            />
+            <span className="popup__error name-error"></span>
+
+            <input
+              type="text"
+              name="specialty"
+              id="specialty"
+              className="popup__input popup__input_el_specialty"
+              required
+              minLength="2"
+              maxLength="200"
+              placeholder="Введите информацию о себе"
+            />
+            <span className="popup__error specialty-error"></span>
+          </fieldset>
+        </PopupWithForm>
+
+        <PopupWithForm
+          name="placeAddForm"
+          title="Новое место"
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+          submitButtonText="Создать">
+          <fieldset className="popup__input-container">
+            <input
+              type="text"
+              name="placeTitle"
+              id="placeTitle"
+              className="popup__input popup__input_el_place-title"
+              required
+              minLength="2"
+              maxLength="30"
+              placeholder="Название"
+            />
+            <span className="popup__error placeTitle-error"></span>
+
+            <input
+              type="url"
+              name="placeLink"
+              id="placeLink"
+              className="popup__input popup__input_el_place-link"
+              required
+              placeholder="Ссылка на картинку"
+            />
+            <span className="popup__error placeLink-error"></span>
+          </fieldset>
+        </PopupWithForm>
+
+        <ImagePopup selectedCardImg={selectedCardImg} onClose={closeAllPopups} />
+
+        <div id="popupConfirmDelCard" className="popup">
+          <div className="popup__container">
+            <button type="button" className="popup__close-button"></button>
+            <form name="confirmDelForm" className="popup__form" noValidate>
+              <h3 className="popup__title">Вы уверены?</h3>
+              <button type="submit" className="popup__button">Да</button>
+            </form>
+          </div>
+        </div>
         <Footer />
       </>
     );
